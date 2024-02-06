abc69878419ddaa77f70ff30b2232c7ad11a21c6
cris-tech96 <147725000+cris-tech96@users.noreply.github.com>
2024-02-06 17:56:22 +0100

    fix

--- springCollection/frameSpring/aop/9)new-AOP/src/main/java/com/example/bean/beanInterface/Ruote.java
+++
@@ -1,6 +0,0 @@
-package com.example.bean.beanInterface;
-
-public interface Ruote {
-    String move();
-    String stop();
-}
--- springCollection/frameSpring/aop/9)new-AOP/src/main/java/com/example/bean/beanInterface/Stereo.java
+++
@@ -1,7 +0,0 @@
-package com.example.bean.beanInterface;
-
-import com.example.bean.Song;
-
-public interface Stereo {
-    String makeSound(Song song);
-}
